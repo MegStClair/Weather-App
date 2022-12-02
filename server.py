298f717c874f1975ae83c4a37782d92da794cb51
Meagan St Clair <stclair.meagan@gmail.com>
2022-12-01 19:16:31 -0600

    successful zipcode API search

--- server.py
+++ server.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request
+
 import os
+import requests
 
 app = Flask(__name__)
 
@@ -15,11 +17,24 @@ def show_weather():
 def search_location():
     """Get location input."""
 
-    location = request.form.get('search')
+    zipcode = request.form.get('search')
 
     key = os.environ['API_KEY']
 
+    url = f'http://api.openweathermap.org/geo/1.0/zip?zip={zipcode}&appid={key}'
+
+    # url = f'http://api.openweathermap.org/geo/1.0/direct?q={location}&limit=1&appid={key}'
+
+    # url = f'https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${key}&units=imperial)'
+
+    # https://api.openweathermap.org/data/2.5/weather?q={city name},{state code}&appid={API key}
+
+    response = requests.get(url)
+    data = response.json()
+    print(data)
+
     return render_template('weather.html')
+
 
 
 if __name__ == '__main__':
