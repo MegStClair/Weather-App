6478034a7b32918e55a55acede77354fc864a03b
Jessica Dea <jessicardea@gmail.com>
2022-12-06 21:58:03 -0800

    Forecast data from api

--- server.py
+++ server.py
@@ -22,7 +22,6 @@ def search_location():
     """Get location input."""
 
     zipcode = request.form.get('search')
-    # export API_KEY='118df5d154ef4d25ebf3ec36bad1180e'
     key = os.environ['API_KEY']
 
     url = f'http://api.openweathermap.org/geo/1.0/zip?zip={zipcode}&appid={key}'
@@ -37,7 +36,6 @@ def search_location():
     url_forecast = f'https://api.openweathermap.org/data/3.0/onecall?lat={lat}&lon={lon}&units=imperial&exclude=hourly,minutely&appid={key}'
 
     response_forecast = requests.get(url_forecast).json()
-    print(response_forecast)
 
     city = data['name']
     print(city)
@@ -66,13 +64,35 @@ def search_location():
     print(datetime.fromtimestamp(sunrise).strftime('%-I:%M %p'))
     print(datetime.fromtimestamp(sunset).strftime('%-I:%M %p'))
 
-   
-    
-    
+    day_1 = round(response_forecast['daily'][1]['temp']['day'])
+    night_1 = round(response_forecast['daily'][1]['temp']['night'])
+    weather_1 = response_forecast['daily'][1]['weather'][0]['main']
+    icon_1 = response_forecast['daily'][1]['weather'][0]['icon']
 
-   
+    print(datetime.fromtimestamp(response_forecast['daily'][1]['dt']))
+
+    day_2 = round(response_forecast['daily'][2]['temp']['day'])
+    night_2 = round(response_forecast['daily'][2]['temp']['night'])
+    weather_2 = response_forecast['daily'][2]['weather'][0]['main']
+    icon_2 = response_forecast['daily'][2]['weather'][0]['icon']
+
+    day_3 = round(response_forecast['daily'][3]['temp']['day'])
+    night_3 = round(response_forecast['daily'][3]['temp']['night'])
+    weather_3 = response_forecast['daily'][3]['weather'][0]['main']
+    icon_3 = response_forecast['daily'][3]['weather'][0]['icon']
+
+    day_4 = round(response_forecast['daily'][4]['temp']['day'])
+    night_4 = round(response_forecast['daily'][4]['temp']['night'])
+    weather_4 = response_forecast['daily'][4]['weather'][0]['main']
+    icon_4 = response_forecast['daily'][4]['weather'][0]['icon']
+
+    print('*******FORECAST*******')
+    print(day_1)
+    print(night_1)
+    print(weather_1)
+    print(icon_1)
+
     return render_template('weather.html')
-
 
 
 if __name__ == '__main__':
